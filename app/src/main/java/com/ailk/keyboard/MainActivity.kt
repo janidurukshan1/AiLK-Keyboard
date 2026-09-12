5d557d15743db6582495e6daebf0f6aaedb0b5bf
janidurukshan1 <janidurukshan300@gmail.com>
2026-09-12 19:42:08 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/ailk/keyboard/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ailk.keyboard
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS))
+    }
+}
